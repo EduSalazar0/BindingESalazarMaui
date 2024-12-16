2362268db3b4897b2aa341c964357a0f2a3182e2
EduSalazar0 <edutebansalazar95@gmail.com>
2024-12-16 08:22:58 -0500

    Se agregaron metodos en la clase EstudianteSQLiteRepository, definición de el nombreDB y _connection

--- BindingESalazarMaui/Repositories/EstudianteUdlaSQLiteRepository.cs
+++ BindingESalazarMaui/Repositories/EstudianteUdlaSQLiteRepository.cs
@@ -1,7 +1,9 @@
 ﻿using BindingESalazarMaui.Interfaces;
 using BindingESalazarMaui.Models;
+using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,20 @@ using System.Threading.Tasks;
 namespace BindingESalazarMaui.Repositories
 {
     public class EstudianteUdlaSQLiteRepository : IEstudianteUdlaRepository
-    { 
+    {
+        private static string NombreBD = "EstudianteUDLA.db3";
+        private SQLiteConnection _connection;
+
+        public EstudianteUdlaSQLiteRepository() //Constructor
+        {
+            Init();
+        }
+
+        public void Init() //Metodo de definicion del path donde se guardara el archivo referente a la base de datos
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory,NombreBD);
+            _connection = new SQLiteConnection(dbPath);
+        }
         public bool ActualizarEstudianteUdla(EstudianteUdla estudiante)
         {
             throw new NotImplementedException();
